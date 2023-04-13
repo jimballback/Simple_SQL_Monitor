9e578f956a2b7a9859aa062a97d48d0d72cfc9fb
jimballback <jimfallback@outlook.com>
2023-04-13 14:22:57 -0500

    Initial performance counter  monitor added.

---
+++ Monitor/job_monitor_iteration.sql
@@ -0,0 +1,21 @@
+﻿/**************************************
+* NAME:	iteration script for the monitor procedures.
+* DESCRIPTION:	To be used as a  one off run or a demo. Otherwise you should use a job scheduler.
+*
+*
+*
+**************************************/
+
+--begin batch
+	DECLARE @iteration_num SMALLINT =1
+	DECLARE @iteration_count SMALLINT =400
+	DECLARE @delay	char(8)= '00:02:00'
+
+WHILE @iteration_num <=@iteration_count
+BEGIN
+
+	exec dbperf.uspDBA_AZURE_Monitor_Performance  @debug =0;
+
+	SELECT @iteration_num = @iteration_num + 1;
+	WAITFOR DELAY @delay;	
+END
