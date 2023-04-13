14f2ab328ba9e16f90b384889aaf79c1655e5f18
jimballback <jimfallback@outlook.com>
2023-04-12 21:11:18 -0500

    Added load scripts

--- Load/data_init_performance_counters.sql
+++ Load/data_init_performance_counters.sql
@@ -2,17 +2,18 @@
 *  Most of the individual database specific counters have been filtered out.
 *  Feel free to just remove the where clause  to load them all, but You will have to remember to  add the new database specific parameters.
 *  DEPENDENCIES: [dbperf].[dba_Instance_perf_param]
-*
+*  --trim required 
+*				SQL Sever 2014+
 *  Original Author: James Nafpliotis
 ************************************************/
 Declare @EngineEdition INT
 SELECT @EngineEdition = CAST(SERVERPROPERTY('EngineEdition') AS INT);
 -- Object_name column has  different values  between PAAS services  and IAAS/On-Prem.
+BEGIN TRY
 IF ((@EngineEdition <= 4 )
 and  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbperf].[dba_Instance_perf_param]') AND type in (N'U'))
 )
 BEGIN
-BEGIN TRY
 --https://learn.microsoft.com/en-us/sql/t-sql/functions/serverproperty-transact-sql?view=sql-server-ver16
 INSERT INTO [dbperf].[dba_Instance_perf_param]
            ([Inst_perf_param_id]
@@ -24,7 +25,7 @@ INSERT INTO [dbperf].[dba_Instance_perf_param]
            ,[Log_ind])
 SELECT 
 	CHECKSUM	(object_name,counter_name,instance_name) AS [Inst_perf_param_id]
-,				object_name,counter_name,instance_name,cntr_type,0,0 AS [Log_ind]
+,				trim(object_name),trim(counter_name),trim(instance_name),cntr_type,0,0 AS [Log_ind]
 
 FROM sys.dm_os_performance_counters
 WHERE  object_name <> 'SQLServer:Databases' 
@@ -37,7 +38,7 @@ AND object_name <> 'SQLServer:Broker Activation'
 UNION
 SELECT 
 	CHECKSUM	(object_name,counter_name,instance_name) AS [Inst_perf_param_id]
-,				object_name,counter_name,instance_name,cntr_type,0,  0 AS [Log_ind]
+,			trim(object_name),trim(counter_name),trim(instance_name),cntr_type,0,0 AS [Log_ind]
 
 FROM sys.dm_os_performance_counters
 WHERE	( object_name = 'SQLServer:Databases'			and instance_name in ('_Total','tempdb') )
@@ -47,18 +48,26 @@ OR		(object_name = 'SQLServer:Query Store'			and instance_name in ('_Total','tem
 OR		(object_name = 'SQLServer:Columnstore'			and instance_name in ('_Total','tempdb') )                                                                                                           
 OR		(object_name = 'SQLServer:Advanced Analytics'	and instance_name in ('_Total','tempdb') )
 OR		(object_name = 'SQLServer:Broker Activation'	and instance_name in ('_Total','tempdb') )
-ORDER BY object_name DESC ,counter_name DESC,instance_name DESC;
+ORDER BY 1 DESC ,2 DESC,3 DESC;
 
 
 ---  Update Parent record, The Base Counters.
-UPDATE dbperf.dba_Instance_perf_param 
-SET parent_param_id = parent.inst_perf_param_id
-FROM dbperf.dba_Instance_perf_param 
-LEFT JOIN dbperf.dba_Instance_perf_param as parent
+
+WITH parent ([Inst_perf_param_id],object_nm,counter_nm,instance_nm,counter_type)
+AS (
+	SELECT [Inst_perf_param_id],object_nm,concat(trim(REPLACE(counter_nm,' (ms)','')),' base') as counter_nm,instance_nm,counter_type
+	FROM[dbperf].[dba_Instance_perf_param]
+	WHERE counter_type ='537003264'
+	)
+UPDATE [dbperf].[dba_Instance_perf_param] set parent_param_id =parent.Inst_perf_param_id
+from parent 
+join  [dbperf].[dba_Instance_perf_param] 
+
 ON dbperf.dba_Instance_perf_param.object_nm = parent.object_nm
 AND dbperf.dba_Instance_perf_param.instance_nm = parent.instance_nm
-AND dbperf.dba_Instance_perf_param.counter_nm=REPLACE(parent.counter_nm,' (ms)','')+ ' base'
+AND dbperf.dba_Instance_perf_param.counter_nm=parent.counter_nm
 WHERE dbperf.dba_Instance_perf_param.counter_type in ( 1073939712 );
+END;
 
 END TRY
 BEGIN CATCH  
@@ -67,7 +76,7 @@ BEGIN CATCH
        ,ERROR_MESSAGE() AS ErrorMessage;  
 END CATCH  
 
-END;
+
 
 
 
---
+++ Load/data_upd_Flag_counters_to_monitor.sql
@@ -0,0 +1,117 @@
+﻿/*  Set the flag counters to perform logging.
+*  
+* 
+*  DEPENDENCIES: [dbperf].[dba_Instance_perf_param]
+*  Original Author: James Nafpliotis
+************************************************/
+UPDATE [dbperf].[dba_Instance_perf_param]
+SET		[Log_ind] = 1
+WHERE  object_nm like '%general statistics'
+  AND	(
+			counter_nm  ='Logins/sec'
+		OR	counter_nm  ='Logouts/sec'
+		OR	counter_nm  ='Connection Reset/sec'
+		OR	counter_nm  ='Temp Tables Creation Rate'
+		OR	counter_nm  ='Processes blocked' 
+		OR  counter_nm  ='User Connections'
+		)   ;
+
+UPDATE [dbperf].[dba_Instance_perf_param]
+SET		[Log_ind] = 1
+WHERE  object_nm like '%buffer manager'
+  AND	(
+			counter_nm  ='Page reads/sec'                                                                                                                  
+		OR	counter_nm  ='Page writes/sec'                                                                                                                 
+		OR	counter_nm  ='Lazy writes/sec'                                                                                                                 
+		OR	counter_nm  ='Checkpoint pages/sec'                                                                                                            
+		OR	counter_nm  ='Buffer cache hit ratio'                                                                                                     
+		OR  counter_nm  ='Page lookups/sec'
+		OR  counter_nm  ='Readahead pages/sec'
+		OR  counter_nm  ='Page life expectancy'                                                                                                            
+		);
+UPDATE [dbperf].[dba_Instance_perf_param]
+SET		[Log_ind] = 1
+WHERE  object_nm like '%Access Methods'
+  AND	(
+			counter_nm  ='Forwarded Records/sec'                                                                                                                  
+		OR	counter_nm  ='Page Splits/sec'                                                                                                                 
+		OR	counter_nm  ='Full Scans/sec'                                                                                                                 
+                                                                                                   
+		);
+UPDATE [dbperf].[dba_Instance_perf_param]
+SET		[Log_ind] = 1
+WHERE  object_nm like '%Locks'
+
+  AND	(	counter_nm  = 'Lock Timeouts/sec'
+		OR	counter_nm  ='Number of Deadlocks/sec'                                                                                                                  
+		OR	counter_nm  ='Lock Wait Time (ms)'                                                                                                                 
+	--	OR	counter_nm  ='Lock Timeouts (timeout > 0)/sec'                                                                                                                 
+                                                                                                   
+		)
+  AND instance_nm ='_Total';
+
+
+UPDATE [dbperf].[dba_Instance_perf_param]
+SET		[Log_ind] = 1
+WHERE  object_nm like '%SQL Errors'
+  AND	(	counter_nm  = 'Errors/sec'
+                                                                                       
+		)
+  AND instance_nm ='_Total';
+
+
+
+UPDATE [dbperf].[dba_Instance_perf_param]
+SET		[Log_ind] = 1
+WHERE  object_nm like '%SQL Statistics'
+
+  AND	(	counter_nm  = 'Batch Requests/sec'
+		OR	counter_nm  ='SQL Compilations/sec'                                                                                                                  
+		OR	counter_nm  ='SQL Re-Compilations/sec'                                                                                                                 
+		OR	counter_nm  ='SQL Attention rate'                                                                                                                 
+                                                                                                   
+		)
+
+
+
+
+
+UPDATE [dbperf].[dba_Instance_perf_param]
+SET		[Log_ind] = 1
+WHERE  object_nm like '%Memory Manager'
+  AND	(	counter_nm  = 'Connection Memory (KB)'
+		OR	counter_nm  = 'Database Cache Memory (KB)'                                                                                                                  
+		OR	counter_nm  = 'Free Memory (KB)'                                                                                                                 
+		OR	counter_nm  = 'Granted Workspace Memory (KB)' 
+       	OR	counter_nm  = 'Lock Memory (KB)'                                                                                                                  
+		OR	counter_nm  = 'Free Memory (KB)'                                                                                                                 
+		OR	counter_nm  = 'Maximum Workspace Memory (KB)'  
+		OR	counter_nm  = 'Optimizer Memory (KB)'                                                                                                                  
+		OR	counter_nm  = 'SQL Cache Memory (KB)'                                                                                                                 
+		OR	counter_nm  = 'Stolen Server Memory (KB)'  
+		OR	counter_nm  = 'Log Pool Memory (KB)'  
+		OR	counter_nm  = 'Target Server Memory (KB)'                                                                                                                  
+		OR	counter_nm  = 'SQL Cache Memory (KB)'                                                                                                                 
+		OR	counter_nm  = 'Total Server Memory (KB)' 
+
+		);
+
+
+UPDATE [dbperf].[dba_Instance_perf_param]
+SET		[Log_ind] = 1
+WHERE  object_nm like '%Wait Statistics'
+  AND	(	counter_nm  = 'Lock waits'
+		OR	counter_nm  = 'Memory grant queue waits'                                                                                                                  
+	--	OR	counter_nm  = 'Thread-safe memory objects waits'                                                                                                                 
+		OR	counter_nm  = 'Log write waits' 
+       	OR	counter_nm  = 'Log buffer waits'                                                                                                                  
+		OR	counter_nm  = 'Network IO waits'                                                                                                                 
+		OR	counter_nm  = 'Page IO latch waits'  
+		OR	counter_nm  = 'Page latch waits'                                                                                                                  
+		OR	counter_nm  = 'Non-Page latch waits'                                                                                                                 
+		OR	counter_nm  = 'Wait for the worker'  
+		OR	counter_nm  = 'Workspace synchronization waits'  
+	--	OR	counter_nm  = 'Transaction ownership waits'                                                                                                                  
+		)
+  AND instance_nm = 'Average wait time (ms)';		
+		
